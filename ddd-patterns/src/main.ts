195064ef5ec91d1121e05681a973185fe6348225
Pedro <pedromotta20@gmail.com>
2023-10-06 22:06:44 -0300

    aplicando tsc antes dos testes

--- ddd-patterns/src/main.ts
+++ ddd-patterns/src/main.ts
@@ -9,8 +9,8 @@ customer.Address = address;
 // RELAÇÃO DE AGREGADO (MESMO AGREGADO)
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item 2", 15, "p2", 2);
 
 const order = new Order("1", "123", [item1, item2]);
 // RELAÇÃO DE REFERÊNCIA POR ID(123) (AGREGADOS DIFERENTES)
