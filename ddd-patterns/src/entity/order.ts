e3b62cebe0a253655016bd8e52dc9d3ed8472b22
Pedro <pedromotta20@gmail.com>
2023-10-06 22:00:28 -0300

    implementando testes

--- ddd-patterns/src/entity/order.ts
+++ ddd-patterns/src/entity/order.ts
@@ -1,13 +1,40 @@
 import OrderItem from "./order_item";
 
 export default class Order {
-  _id: string;
-  _costumerId: string;
-  _items: OrderItem[];
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[];
+  private _total: number;
 
-  constructor(id: string, costumerId: string, items: OrderItem[]) {
+  constructor(id: string, customerId: string, items: OrderItem[]) {
     this._id = id;
-    this._costumerId = costumerId;
+    this._customerId = customerId;
     this._items = items;
+    this._total = this.total();
+    this.validate();
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+
+    if (this._customerId.length === 0) {
+      throw new Error("CustomerId is required");
+    }
+
+    if (this._items.length === 0) {
+      throw new Error("Items are required");
+    }
+
+    if (this._items.some((item) => item.quantity <= 0)) {
+      throw new Error("Quantity must be greater than zero");
+    }
+
+    return true;
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
   }
 }
--- ddd-patterns/src/main.ts
+++ ddd-patterns/src/main.ts
@@ -1,16 +1,16 @@
 import Address from "./entity/address";
-import Customer from "./entity/costumer";
+import Customer from "./entity/customer";
 import Order from "./entity/order";
 import OrderItem from "./entity/order_item";
 
-let costumer = new Customer("123", "Pedro Lugon");
+let customer = new Customer("123", "Pedro Lugon");
 const address = new Address("Rua Dois", 2, "12345-678", "Brasília");
-costumer.Address = address;
+customer.Address = address;
 // RELAÇÃO DE AGREGADO (MESMO AGREGADO)
-costumer.activate();
+customer.activate();
 
 const item1 = new OrderItem("1", "Item 1", 10);
 const item2 = new OrderItem("2", "Item 2", 15);
 
-const order = new Order("1", costumer._id, [item1, item2]);
-// RELAÇÃO DE REFERÊNCIA POR ID (AGREGADOS DIFERENTES)
+const order = new Order("1", "123", [item1, item2]);
+// RELAÇÃO DE REFERÊNCIA POR ID(123) (AGREGADOS DIFERENTES)
