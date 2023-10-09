47cd0d956a3f4cb622b564ac26c94eba076756af
Pedro Lugon <pedromotta20@gmail.com>
2023-10-09 03:45:31 -0300

    domain services

--- ddd-patterns/src/entity/customer.spec.ts
+++ ddd-patterns/src/entity/customer.spec.ts
@@ -50,4 +50,15 @@ describe("Customer unit tests", () => {
 
     expect(customer.isActive()).toBe(false);
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPonts(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPonts(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
--- ddd-patterns/src/entity/customer.ts
+++ ddd-patterns/src/entity/customer.ts
@@ -5,6 +5,7 @@ export default class Customer {
   private _name: string;
   private _address!: Address;
   private _active: boolean = false;
+  private _rewardPoints: number = 0;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -12,8 +13,16 @@ export default class Customer {
     this.validate();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints;
   }
 
   set Address(address: Address) {
@@ -47,5 +56,9 @@ export default class Customer {
 
   deactivate() {
     this._active = false;
+  }
+
+  addRewardPonts(points: number) {
+    this._rewardPoints += points;
   }
 }
