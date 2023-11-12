13d47d914b8dc245ad237f59c2fc909f40ec3edd
Pedro <pedromotta20@gmail.com>
2023-11-12 12:29:33 -0300

    modularizando checkout

--- ddd-patterns/src/domain/checkout/service/order.service.spec.ts
+++ ddd-patterns/src/domain/checkout/service/order.service.spec.ts
@@ -1,7 +1,7 @@
-import Customer from "../entity/customer";
+import Customer from "../../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
-import OrderService from "./order.service";
+import OrderService from "../../service./checkout/service/order.service";
 
 describe("Order service unit tests", () => {
   it("should place a order", () => {
--- ddd-patterns/src/main.ts
+++ ddd-patterns/src/main.ts
@@ -1,7 +1,7 @@
 import Address from "./domain/entity/address";
 import Customer from "./domain/entity/customer";
-import Order from "./domain/entity/order";
-import OrderItem from "./domain/entity/order_item";
+import Order from "./domain/checkout/entity/order";
+import OrderItem from "./domain/checkout/entity/order_item";
 
 let customer = new Customer("123", "Pedro Lugon");
 const address = new Address("Rua Dois", 2, "12345-678", "Brasília");
