560815794b09e93df74822449b3f5ea6c97f930c
Pedro Lugon <pedromotta20@gmail.com>
2023-11-22 02:03:07 -0300

    starting clean arch

---
+++ ddd-patterns/src/usecase/customer/find/find.customer.spec.ts
@@ -0,0 +1,56 @@
+import { Sequelize } from "sequelize-typescript";
+import CustomerModel from "../../../infrastructure/customer/repository/sequelize/customer.model";
+import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
+import Customer from "../../../domain/customer/entity/customer";
+import Address from "../../../domain/customer/value-object/address";
+import FindCustomerUseCase from "./find.customer.usecase";
+
+describe("Test find customer use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([CustomerModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a customer", async () => {
+    const customer = new Customer("123", "John");
+    const address = new Address("Street", 123, "zip", "city");
+    customer.changeAddress(address);
+
+    const customerRepository = new CustomerRepository();
+    await customerRepository.create(customer);
+
+    const input = {
+      id: "123",
+    };
+
+    const usecase = new FindCustomerUseCase(customerRepository);
+
+    const output = {
+      id: "123",
+      name: "John",
+      address: {
+        street: "Street",
+        city: "city",
+        number: 123,
+        zip: "zip",
+      },
+    };
+
+    const result = usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+});
