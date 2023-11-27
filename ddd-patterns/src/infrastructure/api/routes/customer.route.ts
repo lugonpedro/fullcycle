9ddf7a3c2d418fab04d419a62271316a6c44a1e9
Pedro Lugon <pedromotta20@gmail.com>
2023-11-27 15:51:42 -0300

    update customer e2e test

--- ddd-patterns/src/infrastructure/api/routes/customer.route.ts
+++ ddd-patterns/src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import UpdateCustomerUseCase from "../../../usecase/customer/update/update.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -30,6 +31,28 @@ customerRoute.get("/", async (req: Request, res: Response) => {
   const usecase = new ListCustomerUseCase(new CustomerRepository());
   try {
     const output = await usecase.execute({});
+    res.send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+customerRoute.put("/:id", async (req: Request, res: Response) => {
+  const usecase = new UpdateCustomerUseCase(new CustomerRepository());
+
+  try {
+    const customerDto = {
+      id: req.params.id,
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        city: req.body.address.city,
+        number: req.body.address.number,
+        zip: req.body.address.zip,
+      },
+    };
+
+    const output = await usecase.execute(customerDto);
     res.send(output);
   } catch (err) {
     res.status(500).send(err);
