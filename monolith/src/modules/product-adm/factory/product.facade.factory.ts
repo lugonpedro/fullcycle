998c4bc0e815eac4580d142b193e413d422be493
Pedro Lugon <pedromotta20@gmail.com>
2023-12-01 05:11:39 -0300

    feat(product): creating product facade factory

---
+++ monolith/src/modules/product-adm/factory/product.facade.factory.ts
@@ -0,0 +1,18 @@
+import ProductFacade from "../facade/product.facade";
+import ProductRepository from "../repository/sequelize/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import CheckStockProductUseCase from "../usecase/check-stock/check-stock.usecase";
+
+export default class ProductFacadeFactory {
+  static create() {
+    const productRepository = new ProductRepository();
+    const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockProductUseCase(productRepository);
+    const productFacade = new ProductFacade({
+      addUsecase: addProductUseCase,
+      checkStockUsecase: checkStockUseCase,
+    });
+
+    return productFacade;
+  }
+}
