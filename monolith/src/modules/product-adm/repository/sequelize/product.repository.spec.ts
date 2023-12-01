87befac4208ca452ad1aace79aec14815151d20a
Pedro Lugon <pedromotta20@gmail.com>
2023-12-01 04:00:01 -0300

    feat(product): sequelize repository test

--- monolith/src/modules/product-adm/repository/sequelize/product.repository.spec.ts
+++ monolith/src/modules/product-adm/repository/sequelize/product.repository.spec.ts
@@ -40,12 +40,34 @@ describe("Product sequelize repository test", () => {
       where: { id: productProps.id.id },
     });
 
-    const result = productDb.dataValues;
+    expect(productProps.id.id).toEqual(productDb.id);
+    expect(productProps.name).toEqual(productDb.name);
+    expect(productProps.description).toEqual(productDb.description);
+    expect(productProps.purchasePrice).toEqual(productDb.purchasePrice);
+    expect(productProps.stock).toEqual(productDb.stock);
+  });
 
-    expect(productProps.id.id).toEqual(result.id);
-    expect(productProps.name).toEqual(result.name);
-    expect(productProps.description).toEqual(result.description);
-    expect(productProps.purchasePrice).toEqual(result.purchasePrice);
-    expect(productProps.stock).toEqual(result.stock);
+  it("should find a produt", async () => {
+    const productRepository = new ProductRepository();
+
+    ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const product = await productRepository.find("1");
+
+    console.log(product);
+
+    expect(product.id.id).toEqual("1");
+    expect(product.name).toEqual("Product 1");
+    expect(product.description).toEqual("Product 1 description");
+    expect(product.purchasePrice).toEqual(100);
+    expect(product.stock).toEqual(10);
   });
 });
