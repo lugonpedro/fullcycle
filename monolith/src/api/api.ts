8f75c866934d886c72c049c61af77ac0f354be97
Pedro Lugon <pedromotta20@gmail.com>
2023-12-13 04:35:25 -0300

    feat(api): starting api

---
+++ monolith/src/api/api.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import ProductAdmFacadeFactory from "../modules/product-adm/factory/product-adm.facade.factory";
+import { AddProductFacadeInputDto } from "../modules/product-adm/facade/product-adm.facade.dto";
+
+export const addProductRoute = express.Router();
+
+addProductRoute.post("/", async (req: Request, res: Response) => {
+  const facade = ProductAdmFacadeFactory.create();
+
+  try {
+    const input: AddProductFacadeInputDto = {
+      id: req.body.id,
+      name: req.body.name,
+      description: req.body.description,
+      purchasePrice: req.body.purchasePrice,
+      stock: req.body.stock ?? 0,
+    };
+
+    const output = await facade.addProduct(input);
+    res.status(201).send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
