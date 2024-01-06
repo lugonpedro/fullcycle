f2da3bc1700cde76f3366212478d14fd705720d4
Pedro Lugon <pedromotta20@gmail.com>
2024-01-06 12:08:48 -0300

    fix(client): removing unused code

--- monolith/src/modules/client-adm/repository/sequelize/client.repository.ts
+++ monolith/src/modules/client-adm/repository/sequelize/client.repository.ts
@@ -5,18 +5,14 @@ import ClientModel from "./client.model";
 
 export default class ClientRepository implements ClientGateway {
   async add(client: Client): Promise<void> {
-    // try {
-      await ClientModel.create({
-        id: client.id.id,
-        name: client.name,
-        email: client.email,
-        address: client.address,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
-    // } catch (e) {
-    //   console.log(e)
-    // }
+    await ClientModel.create({
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
   }
   async find(id: string): Promise<Client> {
     const client = await ClientModel.findOne({ where: { id } });
